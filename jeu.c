23564e893a05e2f98693a07fa7b27afb859124b9
DamienLeMalETPA <damienlemal123@gmail.com>
2020-02-06 11:14:58 +0100

    Init

---
+++ jeu.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+//random
+
+
+//display en couleurs s'il vous plait
+display(index){
+	switch(index){
+		
+	}
+}
+
+
+
+
+
+//menu vaisseau
+	//voir ressources
+	//voir équipage et compétences
+	//voir niveau des systèmes
+	//voir armes équipées et leurs caractéristiques
+
+//IA combat enemis
+
+
+//menu combat
+	//voir Etat Coque + Bouclier
+		/*
+		___________________________________
+		║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║║
+		¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+		*/
+	//voir Energie disponible
+	//voir actions possibles
+	
+//Menu shop
+	//voir items en vente ( choisis aléatoirement en fonction d'un pool propre à l'avancée et au lieu)
+	//voir caractéristiques des armes et objets
+	
+//Menu scenario
+scenario(index){
+	switch (index){
+		
+	}
+}
+
+
+int main(){
+	//cycle
+		//navigation
+		
+return 0;
+}
