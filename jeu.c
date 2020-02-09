73999ae7427ede08fdf0c0b971781a94e00a8dd0
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-02-09 02:30:15 +0100

    On a la carte et les etapes
    reste à faire les scenario, l'interaction et la veritable navigation

--- jeu.c
+++ jeu.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Scenario{
 	int id;
 	char nom[30];
-}
+};
+typedef struct Scenario Scenario;
 
 //même struct pour les ennemis
 struct Vaisseau{
@@ -18,7 +21,8 @@ struct Vaisseau{
 	//pour les ennemis, correspondra à la valeur du loot
 	int fuel;
 	int flouze;
-}
+};
+typedef struct Vaisseau Vaisseau;
 
 struct Weapon{
 	int id;
@@ -26,39 +30,129 @@ struct Weapon{
 	int atk;
 	int nbr_fire;
 	int accuracy;
-}
+};
+typedef struct Weapon Weapon;
 
+//Donne un point de vue d'ensemble du type de chaque etape pour les dessiner
+struct World{
+	int type[99];
+};
+typedef struct World World;
+
+//Definit chaque etape par son type et les etapes accessibles depuis celle-ci
 struct Etape{
 	int nbr_choix;
-	int milieu1;
-	int milieu2;
-	int milieu3;
-}
+	int milieu;
+	int next[2];
+};
+typedef struct Etape Etape;
+
+//Definit l'agencement des etapes selon un pattern prédéterminé
+struct Route{
+	int road[99];
+	int nbr;
+};
+typedef struct Route Route;
 //random
+int random_nbr (int min, int max){
+	int nbr = 0;
+	nbr = (min + (rand () % (max + 1 - min)));
+	return nbr;
+};
+
+void temp_test_color(){
+	printf("\033[0;31m■■■");
+    printf("\033[1;31m■■■");
+    printf("\033[0;32m■■■");
+    printf("\033[1;32m■■■");
+    printf("\033[0;33m■■■");
+    printf("\033[1;33m■■■");
+    printf("\033[0;34m■■■");
+    printf("\033[1;34m■■■");
+    printf("\033[0;35m■■■");
+    printf("\033[1;35m■■■");
+    printf("\033[0;33m■■■");
+    printf("\033[1;36m■■■");
+}
 
 
 //display en couleurs s'il vous plait
-display(int index, int sub)	{
+void display(int index, int sub)	{
 	switch(index) {
-		//scenario
 		case 1 :
+			//Titre
+			printf("  _________                            __      __               \t"
+				   " /   _____/__________    ____  ____   /  \\    /  \\_____ _______ \t"
+				   " \\_____  \\\\____ \\__  \\ _/ ___\\/ __ \\  \\   \\/\\/   /\\__  \\\\_  __ \\\t"
+				   " /        \\  |_> > __ \\\\  \\__\\  ___/   \\        /  / __ \\|  | \\/\t"
+				   "/_______  /   __(____  /\\___  >___  >   \\__/\\  /  (____  /__|   \t"
+				   "        \\/|__|       \\/     \\/    \\/         \\/        \\/       ");
+
+		//scenario
+		case 2 :
 			switch (sub) {
 				//intro
 				case 1:
-					printf();
+					//printf();
 					break;
 			}
 			break;
 	}
 }
 
+void printMap(World * w, int nbr){
+	char milieuPrint[99][99];
+	for (int i = 0; i < nbr; i++){
+		switch (w->type[i]){
+			case 1:
+				strcpy(milieuPrint[i], "\033[1;33m■\033[0m");
+				break;
+			case 2:
+				strcpy(milieuPrint[i], "\033[0;32m■\033[0m");
+				break;
+			case 3:
+				strcpy(milieuPrint[i], "\033[1;35m■\033[0m");
+				break;
+			case 4:
+				strcpy(milieuPrint[i], "\033[0;34m■\033[0m");
+				break;
+		}
+	}
+	printf("├───────────────────────────────────────────────────────────────┤\n"
+		   "│   0      1      2      3      4      5      6      7      8   │\n"
+		   "├───────────────────────────────────────────────────────────────┤\n"
+		   "│                                                               │\n"
+		   "│                    ┌── %s ──── %s ──┐     ┌── %s ─┐              │\n"
+		   "│      ┌── %s ──── %s ─┴── %s ─┐       ├─ %s ─┴── %s ─┴── %s ──┐      │\n"
+		   "│   %s ─┴── %s ──── %s ──── %s ─┴── %s ─┬┘     ┌── %s ─┐       ├─ %s   │\n"
+		   "│                                  └── %s ─┴── %s ─┴── %s ──┘      │\n"
+		   "│                                                               │\n"
+		   "└───────────────────────────────────────────────────────────────┘\n",
+		   milieuPrint[0],milieuPrint[1],milieuPrint[2],milieuPrint[3],milieuPrint[4],
+		   milieuPrint[5],milieuPrint[6],milieuPrint[7],milieuPrint[8],milieuPrint[9],
+		   milieuPrint[10],milieuPrint[11],milieuPrint[12],milieuPrint[13],milieuPrint[14],
+		   milieuPrint[15],milieuPrint[16],milieuPrint[17],milieuPrint[18]);
+}
 
-//Generate map
-	//for 8 etape
-		//struct etape[i].nbr_choix = rand(2,3);
-		//struct etape[i].milieu1 = rand(1,4) *3;
-		
+void generate_map(){
+	int r = 0;
+	Route route[99];
+	route[1] = ( struct Route ) {{1,2,3,0,4,0,5,6,7,0,8,0,9,0,9,0,10,0,10,11,12,13,14,15,16,0,16,0,17,0,17,0,18,0,18,0,0,0},38};
+	r = random_nbr(1,1);
+	Etape etape[99];
+	World world;
+	//choose route -> i
+	for (int i = 0; i < route[r].nbr; i++){
+		etape[i/2].next[0] = route[r].road[i-1];
+		etape[i/2].next[1] = route[r].road[i];
+	}
+	for (int i = 0; i < route[r].nbr/2; i++){
+		etape[i].milieu = random_nbr(1,4);
+		world.type[i] = etape[i].milieu;
+	}
+	printMap(&world,19);
 	//Niveau --> Debut d'un cycle
+}
 
 //menu vaisseau
 	//voir ressources
@@ -123,7 +217,9 @@ void niveau(){
 }
 
 int main(){
+	srand(time(NULL));
 	int end = 0;
+	generate_map();
 	//cycle
 	while (end == 0){
 		//generate map
